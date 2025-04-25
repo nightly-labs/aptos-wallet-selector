f82ccf83b77a5e91932cf8fab789bb7a4cabb743
LukassF <szkulkerolf@gmail.com>
2025-04-25 18:57:17 +0200

    fix next config

--- apps/nextjs-example/next.config.mjs
+++ apps/nextjs-example/next.config.mjs
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/aptos-wallet-adapter",
-  output: "export",
   images: {
     unoptimized: true,
   },
